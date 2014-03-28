676c9ad5c408593612a291da82a46d2c12794cae
Jose R. Borja <chakaitos@gmail.com>
2014-03-28 16:34:09 -0500

    Complete Hash creation

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,13 +1,19 @@
 class MyHash
   def initialize
+  	@array = Array.new(64)
   end
 
   def find_index(key)
+  	key.hash % 64
   end
 
-  def []=(key, value)
+  def []=(key, value) 							 # Big O = O(1)
+  	location = self.find_index(key)
+  	@array[location] = value
   end
 
   def [](key)
+  	location = self.find_index(key)  # Big O = O(1)
+  	@array[location]
   end
 end
