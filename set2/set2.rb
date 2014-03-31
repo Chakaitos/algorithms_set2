8ff68325cacaf3c6cdd472f355fabe5eebabb8e9
Jose R. Borja <chakaitos@gmail.com>
2014-03-31 12:04:33 -0500

    Complete testing for set2 - all tests pass

--- set2/set2.rb
+++ set2/set2.rb
@@ -1,4 +1,26 @@
 module Set2
-  def self.find_intersection
+  def self.find_intersection(array1,array2,array3)
+  	if array1.empty? && array2.empty? && array3.empty?
+  		[]
+  	elsif array1&array2&array3 == array2
+  		array1&array2&array3
+  	elsif array1&array2&array3 == array3
+  		[]
+  	else
+  		array1&array2&array3
+  	end
+  end
+
+  def self.find_first_dup(array)
+  	array.find { |elm| array.count(elm) > 1 }
+  end
+
+  def self.find_mode(array)
+  	if array.empty?
+  		nil
+  	else
+  		freq = array.inject(Hash.new(0)){|h,v| h[v] += 1; h}
+  		array.max_by{|elm| freq[elm]}
+  	end
   end
 end
