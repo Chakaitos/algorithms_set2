ffc62977215e920c3ffc4a72a96da3f0bd203f08
Jose R. Borja <chakaitos@gmail.com>
2014-04-01 23:29:49 -0500

    Finished rehashing and amortization

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,19 +1,52 @@
 class MyHash
   def initialize
-  	@array = Array.new(64)
+  	@array = Array.new(64){[]}
+    @id = 0
   end
 
   def find_index(key)
-  	key.hash % 64
+  	key.hash % @array.size
   end
 
   def []=(key, value) 							 # Big O = O(1)
-  	location = self.find_index(key)
-  	@array[location] = value
+  	location = find_index(key)
+    is_new_id = true
+
+    @array[location].each do |elem|
+      if elem[0] == key
+        elem[1] = value
+        is_new_id = false
+        return elem[1]   # returns value for matching array
+      end
+    end
+    @array[location] << ([key, value]) 
+    @id += 1 if is_new_id
+    if @id >= (@array.size / 2)
+      new_size = @array.size * 2
+      rehash(new_size)
+    end
   end
 
   def [](key)
-  	location = self.find_index(key)  # Big O = O(1)
-  	@array[location]
+  	location = find_index(key)       # Big O = O(1)
+  	@array[location].each do |elem|
+      if elem[0] == key
+        return elem[1]   # returns value for matching array
+      end
+    end
+    return nil
+  end
+
+  def rehash(new_size)
+    old_array = @array
+    @array = Array.new(new_size){[]}
+
+    old_array.each do |elem|
+      if elem != []
+        elem.each do |x|
+          self[x[0]] = x[1]
+        end
+      end
+    end
   end
 end
